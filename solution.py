33d7348f9e57d0717bfac60d9a89674073681e36
Daria1508 <130033981+Daria1508@users.noreply.github.com>
2023-04-13 11:15:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
+from scipy.stats import mannwhitneyu
 
+chat_id = 1307537098 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, 
+             y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    res = mannwhitneyu(x, y, alternative='two-sided')
+    p_value=res.pvalue
+    if (p_value<0.06):
+        return True
+    else:
+        return False
